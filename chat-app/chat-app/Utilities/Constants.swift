387e022564499cb9cbd52952ae32243b57c84057
mariusz <mariusz.pohorylo@gmail.com>
2017-08-28 23:40:39 +0200

    New controllerview and log in form elements

---
+++ chat-app/chat-app/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  chat-app
+//
+//  Created by mariusz on 28.08.2017.
+//  Copyright © 2017 mariusz pohorylo. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
