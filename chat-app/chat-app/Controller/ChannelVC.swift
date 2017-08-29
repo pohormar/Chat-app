3e38c50a7440229237af4db04b23aa6135f5adf0
mariusz <mariusz.pohorylo@gmail.com>
2017-08-29 22:07:35 +0200

    Add create account page, segues and constraints

--- chat-app/chat-app/Controller/ChannelVC.swift
+++ chat-app/chat-app/Controller/ChannelVC.swift
@@ -20,4 +20,7 @@ class ChannelVC: UIViewController {
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
+    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
+    }
 }
