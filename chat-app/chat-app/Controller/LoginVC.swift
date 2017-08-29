4f2fdb2181b7863e2aa177adf6e66bc419f85a93
mariusz <mariusz.pohorylo@gmail.com>
2017-08-29 20:37:11 +0200

    Add constraints to login page

---
+++ chat-app/chat-app/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  chat-app
+//
+//  Created by mariusz on 29.08.2017.
+//  Copyright © 2017 mariusz pohorylo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
